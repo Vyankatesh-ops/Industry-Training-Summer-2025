8a56fd2227bcacdd5f29a3b075152cc255b5a04f
Vyankatesh Dhotre <vyankateshdhotre1511@gmail.com>
2025-06-11 19:06:10 +0530

    first commit

---
+++ Session1.js
@@ -0,0 +1,139 @@
+
+// js is dynamically typed
+// a = 10
+// b = 'hello java script'
+// console.log(a)
+//let var and const these are keywords to differentiate the variables i.e.  variable declaration 
+// diff between java and js also changes made by ESMCripts6 inn interview
+// ESM Crpits 6 makes updates in js
+
+// a  = 10
+// b = 20
+// c = a + b
+// console.log(c)
+
+// let a = 'Vyankatesh'
+// let b = 11
+// let c = 68.99
+// mutable and reference  types of data type
+//a = 'java'
+//c = 69.00
+// null and undefined. null is lack of value
+// declared and null is considered as value of variable
+// let username  = null
+// console.log(username)
+// updates the username 
+//username  = 'Vyankatesh'
+
+
+// updating the variable method 2 undefined
+// let userPassword ;
+// userPassword = '9370161125'
+// console.log(userPassword)
+
+// let var and const
+// we can update the let and var in resp scope i,e. can be modified
+// but const vaiable can't be modified and can't be re-initialized
+
+
+// const passcode = 1234
+// console.log(passcode)
+// passcode = 5678
+// console.log(passcode)
+// you will get  error while updating the const variable
+
+// here you can update the let variable
+// let a = 10
+// console.log(a)
+// a = 'hello js'
+// console.log(a)
+
+
+// like let var you can update the var type variable
+// var b = 68.99
+// console.log(b)
+// b = 69
+// console.log(b)
+
+// const passcode = Symbol(input)// it will get only unique values
+
+
+// a = 20 // global var
+// function addNumber {
+//     a = 10
+//     console.log(a) // local var output = 10
+// }
+// console.log(a) // output = 20
+
+// console.log("hello java script")
+
+// var name = "Vyankatesh"
+// let location = "Sangli"
+// const phone_no = 9370161125
+
+// name = "Vyankatesh Dhotre"
+// location = "Mars"
+
+// console.log("Name = ",name)
+// console.log("Location = ",location)
+// console.log("Phone Number = ", phone_no)
+
+
+// creating an array student
+
+// let arrStudents = ["Vyankatesh","Salik","Bhushan","Shravan"]
+// console.log(arrStudents) // to print full array 
+
+// console.log(arrStudents[1]) // to print from position number 
+
+// console.log(arrStudents.length) // length  = 4
+
+// second method to declare array
+
+// let arrNew = new Array(4)
+
+// arrNew[0] = "Alex"
+// arrNew[1] = "Lucifer"
+// console.log(arrNew) // [ 'Alex', 'Lucifer', <2 empty items> ] this is output 
+// array size is 4 but it has 2 items so  it shows remaining empty space in array
+
+
+// iterating array with for loop
+// let arrStudents = ["Vyankatesh","Salik","Bhushan","Shravan"]
+// for(let i = 0; i< arrStudents.length;i++)
+// {
+//     console.log(arrStudents[i]) // printing array items
+// }
+
+// assignment : some array methods like sort, arrayStudents.sort()
+
+// sort
+// arrStudents.sort()
+// console.log(arrStudents) // [ 'Bhushan', 'Salik', 'Shravan', 'Vyankatesh' ] output sorted in alphabetical order
+
+// filter
+// reducer 
+
+// shift = removes first element from array and writes the modified array
+// let arrStudents = ["Vyankatesh","Salik","Bhushan","Shravan"]
+// console.log("before shift : ",arrStudents)
+// arrStudents.shift()
+// console.log("after shift : ",arrStudents)
+// output : before shift :  [ 'Vyankatesh', 'Salik', 'Bhushan', 'Shravan' ]
+// after shift :  [ 'Salik', 'Bhushan', 'Shravan' ]
+
+// unshift : adds a new element at first position of array
+// let arrStudents = ["Vyankatesh","Salik","Bhushan","Shravan"]
+// console.log("before unshift : ", arrStudents)
+// arrStudents.unshift("Gojo")
+// console.log("after unshift : ", arrStudents)
+// output :
+// before unshift :  [ 'Vyankatesh', 'Salik', 'Bhushan', 'Shravan' ]
+// after unshift :  [ 'Gojo', 'Vyankatesh', 'Salik', 'Bhushan', 'Shravan' ]
+
+
+
+// slice splice
+
+// just go through it 
+
